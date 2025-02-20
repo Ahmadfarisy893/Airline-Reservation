5710b9ae956157a4d3518bc9ba1d7153c948b703
Ahmadfarisy893 <ahmadhuseinalfarisy@gmail.com>
2025-02-20 20:59:03 +0700

    update header

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "airline_reservation";
+// membuat koneksi ke databse menggunakan mysql
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// cek apkah berhasil terkoneksi
+?>
