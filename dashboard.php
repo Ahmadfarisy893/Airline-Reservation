73ddab591bf51a33415a909ad9279e1f7e4bfd98
Ahmadfarisy893 <ahmadhuseinalfarisy@gmail.com>
2025-02-22 20:58:53 +0700

    Banyak sekali perubahan, capek saya

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<?php
+include (".includes/header.php");
+$title = "Dashboard";
+// menyertakan file untuk menampilakn notifikasi (jika ada) 
+include '.includes/toast_notification.php';
+?>
+
+
+<?php 
+include (".includes/footer.php");
+?>
