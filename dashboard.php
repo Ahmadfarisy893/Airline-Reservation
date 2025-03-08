fb521974f9f3d628581d2ea1fc959206a96112b7
jefrial-rpl <jeff711720@gmail.com>
2025-03-08 14:27:06 +0700

    mengubah nama panggilan di dashboard

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@ include '.includes/toast_notification.php';
                     <div class="d-flex align-items-end row">
                       <div class="col-sm-7">
                         <div class="card-body">
-                          <h5 class="card-title text-primary">Selamat Datang Ahmad! 🎉</h5>
+                          <h5 class="card-title text-primary">Selamat Datang <?php echo $nama; ?>🎉</h5>
                           <p class="mb-4">
                             You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
                             your profile.
